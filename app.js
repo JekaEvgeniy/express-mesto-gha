3571eca52ca36f891037d5d64603cd0c3f28d4e8
JekaEvgeniy <pruven@yandex.ru>
2023-07-23 23:42:51 +0300

    feat: get/post users

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log('Test13!2');
+const express = require('express');
+// const bodyParser = require('body-parser');
+const app = express();
+
+const users = [];
+let id = 0;
+
+// app.use(bodyParser.json());
+app.use(express.json());
+
+app.get('/users', (req, res) => {
+  console.log('GET /users');
+  res.status(200).send(users);
+});
+
+app.get('/users/:id', (req, res) => {
+	let { id } = req.params;
+	const user = users.find(item => item.id === Number(id));
+	if ( user ){
+		res.send( user );
+	}else {
+		res.status(404).send({message: 'User not found'});
+	}
+
+});
+
+app.post('/users', (req, res) => {
+  console.log('POST /users');
+
+	id += 1;
+  const newUser = {
+    id,
+		...req.body,
+  };
+
+  users.push(newUser);
+
+	res.status(201).send(newUser);
+});
+
+app.listen(3000, () => {
+  console.log('listen port 3000');
+});
