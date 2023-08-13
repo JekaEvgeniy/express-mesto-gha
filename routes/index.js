3525caa9cfd85bc15c6294f07037015c65d48b2b
JekaEvgeniy <pruven@yandex.ru>
2023-08-14 02:10:49 +0300

    autotest: page404

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,10 @@ const cardRoutes = require('./cards');
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
 
+router.use('*', (req, res) => {
+  res.status(codeErrors.notFound).send({
+    message: 'Запрашиваемой страницы нет!',
+  });
+});
+
 module.exports = router;
