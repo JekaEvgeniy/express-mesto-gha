09a61a84ad554efd39411067cb1710b87d16c373
JekaEvgeniy <pruven@yandex.ru>
2023-08-13 22:24:08 +0300

    update: eslint

--- routes/cards.js
+++ routes/cards.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-const { getCards, createCard, removeCard, getCardById, likeCard, dislikeCard } = require('../controllers/cards');
+const {
+  getCards, createCard, removeCard, getCardById, likeCard, dislikeCard,
+} = require('../controllers/cards');
 
 router.get('/', getCards);
 router.get('/:id', getCardById);
