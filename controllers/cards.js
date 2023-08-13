950d76b223e09194c1d54c731102879200af610d
JekaEvgeniy <pruven@yandex.ru>
2023-08-13 15:46:34 +0300

    feat: add get card ID

--- controllers/cards.js
+++ controllers/cards.js
@@ -29,4 +29,63 @@ const createCard = (req, res) => {
     });
 };
 
-module.exports = { getCards, createCard };
+const removeCard = (req, res) => {
+  Card.findByIdAndRemove(req.params.cardId)
+    .orFail(() => {
+      const error = new Error(textError);
+      error.statusCode = 404;
+      return error;
+    })
+    .then((card) => {
+      if (! card ) {
+        res.status(404).send({ message: 'Такой карточки не существует' })
+      }else {
+        res.send({ data: card });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Ошибка. Проверьте переданный ID' })
+      }else {
+        res.status(500).send({
+          message: 'Internal Server Error',
+          err: err.message,
+          stack: err.stack,
+        });
+      }
+    });
+};
+
+const getCardById = (req, res) => {
+  const textError = 'Not Found';
+
+  Card.findById(req.params.id)
+    .orFail(() => {
+      const error = new Error(textError);
+      error.statusCode = 404;
+      return error;
+    })
+    .then((card) => {
+      if (card) {
+        res.status(200).send(card);
+      } else {
+        res.status(404).send({ message: 'Нет карточки с таким ID' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: `Ошибка ${err}` });
+      } else if (err.name === textError) {
+        res.status(404).send({ message: 'Нет карточки с таким ID' });
+      } else {
+        res.status(500).send({
+          message: 'Internal Server Error',
+          err: err.message,
+          stack: err.stack,
+        });
+      }
+    });
+};
+
+
+module.exports = { getCards, createCard, removeCard, getCardById };
--- routes/cards.js
+++ routes/cards.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
-const { getCards, createCard } = require('../controllers/cards');
+const { getCards, createCard, removeCard, getCardById } = require('../controllers/cards');
 
 router.get('/', getCards);
+router.get('/:id', getCardById);
+
 router.post('/', createCard);
+
+router.delete('/:id', removeCard);
+
 module.exports = router;
