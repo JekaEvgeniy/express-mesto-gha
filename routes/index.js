5e08e22b939a61224776ab775b93ac2bf8da058d
JekaEvgeniy <pruven@yandex.ru>
2023-09-16 22:08:02 +0300

    feat: Joi

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,36 @@
 const router = require('express').Router();
+const { celebrate, Joi, errors } = require('celebrate'); // https://www.npmjs.com/package/celebrate
+
 const { codeErrors } = require('../vars/data');
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 const { createUser, getCurrentUser, login } = require('../controllers/users');
 const auth = require('../widdlewares/auth');
 
-router.post('/signup', createUser);
-router.post('/signin', login);
+// https://regex101.com/
+// ТЗ: Поле password не ограничено в длину, так как пароль хранится в виде хеша
+router.post('/signup',
+  celebrate({
+    body: Joi.object().keys({
+      password: Joi.string().required(),
+      email: Joi.string().email().required(),
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().regex(/^(http|https):\/\/(?:www\.)?[a-zA-Z0-9._~\-:?#[\]@!$&'()*+,/;=]{2,256}\.[a-zA-Z0-9./?#-]{2,}$/),
+    }),
+  }),
+  createUser,
+);
+
+router.post('/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login,
+);
 
 router.use(auth);
 
